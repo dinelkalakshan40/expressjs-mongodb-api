2407c1fdfb65af80ddcbae09820e3c7906cf326b
dinelka <newlakshan3@gmail.com>
2025-03-27 09:56:28 +0530

    update and deleted

--- router/userRouter.js
+++ router/userRouter.js
@@ -36,4 +36,32 @@ router.get("/:email",async (req,res) =>{
     }
 })
 
+router.put("/:email",async (req,res)=>{
+    try {
+        const user = await User.findOneAndUpdate(
+            { email: req.params.email }, // Find by email
+            req.body,                    // Update with request body
+            { new: true }                 // Return updated user
+        );
+        if (!user){
+            return res.status(404).json({message:"user not found"});
+        }
+        return res.status(200).json(user);
+    }catch (err){
+        res.status(500).json({error:err.message});
+    }
+});
+
+router.delete("/:email",async (req,res)=>{
+    try {
+        const user =await User.findOneAndDelete({email: req.params.email});
+        if(!user){
+            return res.status(404).json({message:"User not found"});
+        }
+        res.status(200).json({message:"user deleted"})
+    }catch (err){
+        res.status(500).json({error:err.message});
+    }
+})
+
 module.exports = router;
